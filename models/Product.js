85302d75ccade603158209ab5681f44611c36047
hhq85 <hhq85@gmail.com>
2020-12-19 15:02:58 +0700

    Refactoring code

--- models/Product.js
+++ models/Product.js
@@ -1,23 +1,28 @@
-const products = [];
 const fs = require('fs');
 const path = require('path');
 
+const p = path.join(
+	path.dirname(process.mainModule.filename),
+	'data',
+	'product.json'
+);
+
+const getProductFromFile = (cb) => {
+	fs.readFile(p, (err, fileContent) => {
+		if (err) {
+			cb([]);
+		} else {
+			cb(JSON.parse(fileContent));
+		}
+	});
+};
 module.exports = class Product {
 	constructor(title) {
 		this.title = title;
 	}
 
 	save() {
-		const p = path.join(
-			path.dirname(process.mainModule.filename),
-			'data',
-			'product.json'
-		);
-		fs.readFile(p, (err, fileContent) => {
-			let products = [];
-			if (!err) {
-				products = JSON.parse(fileContent);
-			}
+		getProductFromFile((products) => {
 			products.push(this);
 			fs.writeFile(p, JSON.stringify(products), (err) => {
 				console.log(err);
@@ -26,16 +31,6 @@ module.exports = class Product {
 	}
 
 	static fetchAllData(callback) {
-		const p = path.join(
-			path.dirname(process.mainModule.filename),
-			'data',
-			'product.json'
-		);
-		fs.readFile(p, (err, fileContent) => {
-			if (err) {
-				callback([]);
-			}
-			callback(JSON.parse(fileContent));
-		});
+		getProductFromFile(callback);
 	}
 };
